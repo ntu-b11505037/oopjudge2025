26a55d2f112b3c29bf0f0512734712d985c17f4d
ntu-b11505037 <b11505037@ntu.edu.tw>
2025-03-14 16:32:21 +0800

    Update GreenCrud.java

--- 3/GreenCrud.java
+++ 3/GreenCrud.java
@@ -2,17 +2,16 @@
 public class GreenCrud {
 
 	public static int calPopulation(int initialSize,int days) {
-		int a,b,total=0;
-		a=initialSize;
-		b=initialSize;
-		
-		for(int number=days/5;number>1;number--) {
-			
-			total=a+b;
-			a=b;
-			b=total;
-		}
-		return total;
+		int a = initialSize;
+        int b = initialSize;
+        int c = 0;
+        
+        for (int i = 10; i <= days; i += 5) {
+            c = a + b;
+            a = b;
+            b = c;
+        	}
+        
+	return b;
 	}
-	
 }
