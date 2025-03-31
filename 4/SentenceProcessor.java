0d5b1212a8797c556d61d9f8e623273c5618f4f0
ntu-b11505037 <b11505037@ntu.edu.tw>
2025-04-01 02:34:43 +0800

    Update SentenceProcessor.java

--- 4/SentenceProcessor.java
+++ 4/SentenceProcessor.java
@@ -1,38 +1,34 @@
 
 public class SentenceProcessor {
-	
-	public static String removeDuplicateWords(String sentence){
-		
-		String[] words = sentence.trim().split("\\s+");
-		String result="";
-		
-		for(int i=0;i<words.length;i++) {
-			boolean isDuplicate = false;
-			for(int j=0;j<i;j++) {
-				if(words[i].equals(words[j])) {
-					isDuplicate=true;
-					break;
-				}
-			}
-			if(!isDuplicate) {
-				result+=(result.isEmpty()?"":" ")+words[i];
-			}
-		}
-		
-		return result;
-		
-	}
-	
+    public static String removeDuplicatedWords(String sentence) {
+        String[] words = sentence.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
 
-	public static String replaceWord(String target,String replacement,String sentence){
-		
-		String[] words = sentence.trim().split("\\s+");
-		String result="";
-		for(int i=0;i<words.length;i++) {
-			result+=(i>0?" ":"")+(words[i].equals(target)?replacement:words[i]);
-		}
-		 return result;
-		
-	}
-	
+        for (int i = 0; i < words.length; i++) {
+            boolean isDuplicate = false;
+            for (int j = 0; j < i; j++) {
+                if (words[i].equals(words[j])) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                if (result.length() > 0) result.append(" ");
+                result.append(words[i]);
+            }
+        }
+        return result.toString();
+    }
+
+    public static String replaceWord(String oldWord, String newWord,String sentence) {
+        String[] words = sentence.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) result.append(" ");
+            result.append(words[i].equals(oldWord) ? newWord : words[i]);
+        }
+        return result.toString();
+    }
+
 }
